660b37eb64b5fc86de41924fe09860bb5367babd
Mattia Centini <mattia.centini@gmail.com>
2020-06-25 11:59:54 +0200

    adjusted style

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const textNode = document.getElementById("form");
 function addList() {
   //CREATING EACH DIV BLOCK containing the added todo text
   const addedTodo = document.createElement("div");
+  addedTodo.className = "todo";
   const li = document.createElement("li");
   li.innerText = textNode.value;
   addedTodo.appendChild(li);
-  // complete button
-  const completeBtn = document.createElement("span");
-  completeBtn.className = "fas fa-check-square";
-  li.appendChild(completeBtn);
-  // delete button
-  const deleteBtn = document.createElement("span");
-  deleteBtn.className = "fas fa-trash-alt";
-  li.appendChild(deleteBtn);
+
+  //delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteBtn.className = "delete-box";
+  addedTodo.appendChild(deleteBtn);
+
+  //complete button
+  const completeBtn = document.createElement("button");
+  completeBtn.innerHTML = '<i class="fas fa-check-square"></i>';
+  completeBtn.className = "complete-box";
+  addedTodo.appendChild(completeBtn);
+
+  // // complete button
+  // const completeBtn = document.createElement("span");
+  // completeBtn.className = "fas fa-check-square";
+  // li.appendChild(completeBtn);
+  // // delete button
+  // const deleteBtn = document.createElement("span");
+  // deleteBtn.className = "fas fa-trash-alt";
+  // li.appendChild(deleteBtn);
+
   //append div to ul
   const list = document.querySelector(".list");
   list.appendChild(addedTodo);
   textNode.value = " ";
-  checked();
-  deleted();
+  //add delete button
 }
 
 function createTodo() {
@@ -32,24 +46,11 @@ function createTodo() {
 }
 createTodo();
 
-//add a line-through when clicking on complete
-const checked = () => {
-  const check = document.querySelector(".fa-check-square");
-  check.addEventListener("click", () => {
-    let li = document.querySelector("li");
-    li.style.textDecoration = "line-through";
-  });
-};
-
-//delete row when clicking on trash
-const deleted = () => {
-  const trash = document.querySelector(".fa-trash-alt");
-  trash.addEventListener("click", () => {
-    let li = document.querySelector("li");
-    li.style.display = "none";
-  });
-};
-
+// liItems.forEach((item, index) => {
+//   if (item[index] >= 0) {
+//     item.className = "fas fa-trash-alt";
+//   }
+// });
 //
 // use loops to iterate through li items
 // queryselectorAll (li elements) [...]
