cc200c8a4ab52543c82e528f2b517d5266f100a1
Mattia Centini <mattia.centini@gmail.com>
2020-07-03 16:48:48 +0200

    added functions to remove and complete li, added enter key listener

--- app.js
+++ app.js
@@ -1,56 +1,71 @@
-const addBtn = document.querySelector(".addBtn");
-const textNode = document.getElementById("form");
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector(".addBtn");
+  const textNode = document.getElementById("form");
 
-function addList() {
-  //CREATING EACH DIV BLOCK containing the added todo text
-  const addedTodo = document.createElement("div");
-  addedTodo.className = "todo";
-  const li = document.createElement("li");
-  li.innerText = textNode.value;
-  addedTodo.appendChild(li);
+  // Create a "close" button and append it to each list item
 
-  //delete button
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.className = "delete-box";
-  addedTodo.appendChild(deleteBtn);
+  function addList() {
+    //CREATING EACH DIV BLOCK containing the added todo text
+    const addedTodo = document.createElement("div");
+    addedTodo.className = "todo";
+    const li = document.createElement("li");
+    li.innerText = textNode.value;
+    addedTodo.appendChild(li);
 
-  //complete button
-  const completeBtn = document.createElement("button");
-  completeBtn.innerHTML = '<i class="fas fa-check-square"></i>';
-  completeBtn.className = "complete-box";
-  addedTodo.appendChild(completeBtn);
+    //delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteBtn.className = "delete-box";
+    addedTodo.appendChild(deleteBtn);
+    //add an event to delete the li
+    deleteBtn.addEventListener("click", removeItem);
 
-  // // complete button
-  // const completeBtn = document.createElement("span");
-  // completeBtn.className = "fas fa-check-square";
-  // li.appendChild(completeBtn);
-  // // delete button
-  // const deleteBtn = document.createElement("span");
-  // deleteBtn.className = "fas fa-trash-alt";
-  // li.appendChild(deleteBtn);
+    //complete button
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = '<i class="fas fa-check-square"></i>';
+    completeBtn.className = "complete-box";
+    addedTodo.appendChild(completeBtn);
+    //add an event to complete the li
+    completeBtn.addEventListener("click", completeItem);
 
-  //append div to ul
-  const list = document.querySelector(".list");
-  list.appendChild(addedTodo);
-  textNode.value = " ";
-  //add delete button
-}
+    //append div to ul
+    const list = document.querySelector(".list");
+    list.appendChild(addedTodo);
+    textNode.value = " ";
+  }
 
-function createTodo() {
+  //add list item when clicking on + button
   addBtn.addEventListener("click", () => {
     return textNode.value === " "
       ? alert("you need to input something")
       : addList();
   });
-}
-createTodo();
 
-// liItems.forEach((item, index) => {
-//   if (item[index] >= 0) {
-//     item.className = "fas fa-trash-alt";
-//   }
-// });
-//
-// use loops to iterate through li items
-// queryselectorAll (li elements) [...]
+  //add list item when pressing enter key
+  textNode.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector(".addBtn").click();
+    }
+  });
+
+  //turn background li green when pressing check button
+  function completeItem() {
+    //grab the div container
+    let item = this.parentNode;
+    console.log(item);
+    return item.className === "todo"
+      ? (item.style.backgroundColor = "green")
+      : console.log("ciao");
+  }
+
+  //remove li when pressing trash button
+  function removeItem() {
+    //grab the div container
+    let item = this.parentNode;
+    console.log(item);
+    return item.className === "todo"
+      ? (item.style.display = "none")
+      : console.log("ciao");
+  }
+});
